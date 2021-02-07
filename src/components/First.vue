d6020066162a6e5740537569a44c2614ee8f5a59
TQ <756342530@qq.com>
2021-02-07 14:05:43 +0800

    init vue-router

---
+++ src/components/First.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>first</div>
+</template>
+
+<script lang="ts">
+
+  export default  {
+
+  }
+</script>
+
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,18 @@ import {createApp} from 'vue';
 import App from './App.vue';
 import './index.css';
 import {createWebHashHistory, createRouter} from 'vue-router';
-
+import First from './components/First.vue'
+import Second from './components/Second.vue'
 const history = createWebHashHistory();
 const router = createRouter({
   history,
   routes: [
+    {path:'/',component:First},
+    {path:'/second',component:Second},
   ],
 });
 
-createApp(App).mount('#app');
+const app = createApp(App)
+app.use(router)
+app.mount('#app');
+
