d8d678c814ea176b38c176f07432aa05b6603c56
TQ <756342530@qq.com>
2021-02-08 22:23:05 +0800

    完成 Button 组件默认样式，CSS 最小影响原则

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="muji-button" :class="'theme-'+ theme">
+  <button :class="'muji-theme-'+ theme" class="muji-button">
     <slot/>
   </button>
 </template>
@@ -13,7 +13,7 @@
     },
   };
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
   $h: 32px;
   $border-color: #d9d9d9;
   $color: #333;
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
+import './index.scss';
+import './lib/muji.scss';
 import {createApp} from 'vue';
 import App from './App.vue';
-import './index.scss';
-
 import router from './router';
-
 
 const app = createApp(App);
 app.use(router);
