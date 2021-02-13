661270b7024f74b95a9052722025c4c47f3fd469
TQ <756342530@qq.com>
2021-02-13 16:38:06 +0800

    完成检查子组件类型功能

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,9 +1,22 @@
 <template>
-  <div>Tabs</div>
+  <component :is="defaults[0]"/>
+  <component :is="defaults[1]"/>
 </template>
 <script lang="ts">
+  import Tab from './Tab.vue';
 
-  export default {};
+  export default {
+    setup(props, context) {
+      const defaults = context.slots.default();
+      defaults.forEach((tag) => {
+        if (tag.type !== Tab) {
+          throw new Error('Tabs子标签必须是Tab');
+        }
+      });
+      return {defaults};
+    }
+  };
 </script>
 <style lang="scss">
+
 </style>
