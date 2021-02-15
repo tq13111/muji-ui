ac2882e775c23090dca3a707cc185f30507f7b42
TQ <756342530@qq.com>
2021-02-15 18:29:10 +0800

    小优化，使用 component

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -3,7 +3,7 @@
     <div class="demo ">
       <h2>常规用法</h2>
       <div class="demo-component">
-        <Switch1Demo></Switch1Demo>
+        <component :is="Switch1Demo"></component>
       </div>
       <div class="demo-actions ">
         <Button>查看代码</Button>
@@ -15,7 +15,7 @@
     <div class="demo ">
       <h2>支持 disabled </h2>
       <div class="demo-component">
-        <Switch2Demo></Switch2Demo>
+        <component :is="Switch2Demo"></component>
       </div>
       <div class="demo-actions">
         <Button>查看代码</Button>
@@ -27,14 +27,13 @@
   </div>
 </template>
 <script lang="ts">
-  import Switch from '../lib/Switch.vue';
   import Button from '../lib/Button.vue';
   import {ref} from 'vue';
   import Switch1Demo from './Switch1.demo.vue';
   import Switch2Demo from './Switch2.demo.vue';
 
   export default {
-    components: {Switch1Demo, Switch2Demo, Switch, Button},
+    components: {Button},
     setup() {
       const bool = ref(false);
       return {bool, Switch1Demo, Switch2Demo};
